000fc2d3025fbaa4e779c777ccf60d2fedeb5274
Babita de Boer <Babita de Boer>
2017-09-04 12:58:33 +0200

    added pizzas.php

---
+++ pizzas.php
@@ -0,0 +1,27 @@
+{
+  "records": [
+    {
+      "Name": "Margerita",
+      "Price": 10.00,
+	  "Count": 0,
+      "Calories": "324"
+    },
+    {
+     "Name": "Pepperoni",
+      "Price": 13.00,
+	  "Count": 0,
+      "Calories": 324
+    },
+    {"Name": "Hawaii",
+      "Price": 12.00,
+	  "Count": 0,
+      "Calories": 324
+    },
+    {
+     "Name": "Shoarma",
+      "Price": 10.00,
+	  "Count": 0,
+      "Calories": 500
+    }
+  ]
+}
