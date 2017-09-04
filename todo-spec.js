9c78e77579c80a794e1a0afd1cacd264ac694f7b
Babita de Boer <Babita de Boer>
2017-09-04 09:47:34 +0200

    Added bind to all pizzas

--- todo-spec.js
+++ todo-spec.js
@@ -1,13 +1,27 @@
-/* Test 3 - pizza count should be 1 after pizza is added */
+/* Test E2E-01 - pizza totalPrice should be 10.00 after Margerita pizza is added */
 	describe('addPizza', function () {
-		browser.get('http://www.filehosting.org/file/details/690967/Opgave%20Deel%201.html');
+		browser.get('https://rawgit.com/bdeboer95/Workshop-1-Deel-1/master/Opgave%20Deel%202.html');
 
-     it('should add one and two', function() {
-    browser.get('http://www.filehosting.org/file/details/690967/Opgave%20Deel%201.html');
+     it('should be 10 euros', function() {
+    browser.get('https://rawgit.com/bdeboer95/Workshop-1-Deel-1/master/Opgave%20Deel%202.html');
 
     element(by.id('add-Margerita')).click();
 
     expect(element(by.binding('totalPrice')).getText()).
-        toEqual('10'); // Total price must equal 10 
+        toEqual('10.00'); // Total price must equal 10 
+  });
+	});
+	
+	/* Test E2E-02 - pizza count should be 1 after Pepperoni pizza is added */
+	describe('updatePizzaCount', function () {
+		browser.get('https://rawgit.com/bdeboer95/Workshop-1-Deel-1/master/Opgave%20Deel%202.html');
+
+     it('should add 1 pepperoni pizza', function() {
+    browser.get('https://rawgit.com/bdeboer95/Workshop-1-Deel-1/master/Opgave%20Deel%202.html');
+
+    element(by.id('add-Pepperoni')).click();
+
+    expect(element(by.binding('Pepperoni')).getText()).
+        toEqual('1'); // Total count for pepperoni must equal 1
   });
 	});
